ada581916a88b62c943839a308a962b5f6d28796
sohib abed <sohibabed13@gmail.com>
2021-09-29 21:25:38 +0300

    first commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,28 @@
+export const SliderData =[
+    {
+        image:'https://media.istockphoto.com/photos/young-man-arms-outstretched-by-the-sea-at-sunrise-enjoying-freedom-picture-id1285301614?b=1&k=20&m=1285301614&s=170667a&w=0&h=tDEC2-p91cZiNU5C19sVhB9L08PmaH5wIijCvRDalCI='
+    },
+
+    {
+        image:'https://images.unsplash.com/photo-1517400508447-f8dd518b86db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        image:'https://images.unsplash.com/photo-1517999349371-c43520457b23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        image:'https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1527151977613-a89904713c47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+
+    {
+        image:'https://media.istockphoto.com/photos/couple-enjoy-some-red-wine-in-ancient-italian-castle-picture-id1291807006?b=1&k=20&m=1291807006&s=170667a&w=0&h=2Q5eiUYzXM59mKR-L-mivNR68ygmiVSuQN2q2CARAJc='
+    }, {
+        image:'https://media.istockphoto.com/photos/romantic-couple-on-summer-vacation-enjos-the-sunset-over-the-sea-by-picture-id1256296335?b=1&k=20&m=1256296335&s=170667a&w=0&h=qHMizjo19yp4P2IolVNI3zCndbeKqv2xHiU2ZZY2DSM='
+    },
+
+    
+]
